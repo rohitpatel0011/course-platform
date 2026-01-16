f2e45ac1475aa40a108104a14fe291cfd5750c97
RohitPatel00111 <rajgolu636@gmail.com>
2026-01-16 11:17:38 +0530

    fix CROS

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 // CORS Configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', "https://course-platform-one.vercel.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
